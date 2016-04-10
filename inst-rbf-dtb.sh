8ba9b0d594e71e75b5598e9fce3e34763c86f847
the-snowwhite <producer@holotronic.dk>
2016-04-10 03:27:55 +0200

    updated sd install script to select nano, de1 or sockit sd-bootfiles-install
    Signed-off-by: the-snowwhite <producer@holotronic.dk>

--- inst-rbf-dtb.sh
+++ inst-rbf-dtb.sh
@@ -2,46 +2,63 @@
 
 projdirname=mksocfpga/HW/QuartusProjects
 projects=$(ls ../$projdirname)
-folder=DE0_NANO_SOC_GHRD
-#folder=SoCkit_GHRD
+nanofolder=DE0_NANO_SOC_GHRD
+sockitfolder=SoCkit_GHRD
+de1folder=DE1_SOC_GHRD
+
+#mkdir -p boot_files/folder1
+#mkdir -p boot_files/folder2
+for folder in $projects
+
+do
+         mkdir -p boot_files/$folder
+
+        cp -v ../$projdirname/$folder/soc_system.dtb boot_files/$folder/socfpga.dtb
+        cp -v ../$projdirname/$folder/soc_system.dts boot_files/$folder/socfpga.dts
+        cp -v ../$projdirname/$folder/output_files/soc_system.rbf boot_files/$folder/socfpga.rbf
+        echo "Boot files copied to: ${folder}"
+done
+
 set -e  # exit on all errors
-
-mkdir -p boot_files
-#for folder in $projects
-
-#do
-#        tar -zxvf ../QuartusProjects/$folder/sd_fat.tar.gz soc_system.dtb
-#        mv soc_system.dtb $folder.dtb
-#        tar -zxvf ../QuartusProjects/$folder/sd_fat.tar.gz boot_files/*.rbf
-#        mv boot_files/*.rbf boot_files/$folder.rbf
-
-        cp -v ../$projdirname/$folder/soc_system.dtb boot_files/socfpga.dtb
-        cp -v ../$projdirname/$folder/soc_system.dts boot_files/socfpga.dts
-        cp -v ../$projdirname/$folder/output_files/soc_system.rbf boot_files/socfpga.rbf
-#done
 
 
 if [ "$1" != "" ]; then
-        lsblk
-        echo ""
-        echo "files will be installed on $1"
-        echo ""
-        sudo mkdir -p /mnt/rootfs
-#        sudo mount -o rw -o uid=1000,gid=1000 $1 /mnt/boot
-        sudo mount $1 /mnt/rootfs
-#        sudo cp *.dtb /mnt/boot
-        sudo cp -fv boot_files/socfpga* /mnt/rootfs/boot
-#        sudo cp u-boot.scr /mnt/boot
-        sync
-        sudo umount /mnt/rootfs
-        sync
-        echo "operation finished"
+   lsblk
+   echo ""
+   echo "files will be installed on $1"
+   echo ""
+   if [ "$2" != "" ]; then
+      if [ "$2" == "nano" ]; then
+         cpfolder=${nanofolder}
+      elif [ "$2" == "de1" ]; then
+         cpfolder=${de1folder}
+      elif [ "$2" == "sockit" ]; then
+         cpfolder=${sockitfolder}
+      fi
+   fi
+   if [ "$cpfolder" != "" ]; then
+      sudo mkdir -p /mnt/rootfs
+      sudo mount $1 /mnt/rootfs
+      sudo cp -fv boot_files/${cpfolder}/socfpga* /mnt/rootfs/boot
+      sync
+      sudo umount /mnt/rootfs
+#     sudo mkdir -p /mnt/boot
+#     sudo mount -o rw -o uid=1000,gid=1000 $1 /mnt/boot
+#     sudo cp -fv boot_files/socfpga* /mnt/boot
+#     sync
+#     sudo umount /mnt/boot
+      sync
+      echo "operation finished sucessfully"
+   else
+      echo "wrong board install option use: [/dev/sdxx] [nano or de1 or sockit]"
+   fi
 else
-        lsblk
-        echo ""
-        echo "! -- not installing files"
-        echo "no installation drive given"
-        echo "ie: /dev/sdxx"
-        echo ""
+   lsblk
+   echo ""
+   echo "! -- no options given"
+   echo "will only copy files to boot_folder"
+   echo "to install files to sd"
+   echo "use: [/dev/sdxx] [nano | de1 | sockit]"
+   echo ""
 fi
 
