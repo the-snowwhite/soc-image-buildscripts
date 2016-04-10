f225e9ab39f80c238978f4bac186664b23cfd423
the-snowwhite <producer@holotronic.dk>
2016-04-10 16:44:29 +0200

    updated sd boot file install script, added fpga configure script with gpio remove fix
    Signed-off-by: the-snowwhite <producer@holotronic.dk>

--- inst-rbf-dtb.sh
+++ inst-rbf-dtb.sh
@@ -8,49 +8,50 @@ de1folder=DE1_SOC_GHRD
 
 #mkdir -p boot_files/folder1
 #mkdir -p boot_files/folder2
-for folder in $projects
-
-do
-         mkdir -p boot_files/$folder
-
-        cp -v ../$projdirname/$folder/soc_system.dtb boot_files/$folder/socfpga.dtb
-        cp -v ../$projdirname/$folder/soc_system.dts boot_files/$folder/socfpga.dts
-        cp -v ../$projdirname/$folder/output_files/soc_system.rbf boot_files/$folder/socfpga.rbf
-        echo "Boot files copied to: ${folder}"
-done
-
 set -e  # exit on all errors
 
-
 if [ "$1" != "" ]; then
-   lsblk
-   echo ""
-   echo "files will be installed on $1"
-   echo ""
-   if [ "$2" != "" ]; then
-      if [ "$2" == "nano" ]; then
-         cpfolder=${nanofolder}
-      elif [ "$2" == "de1" ]; then
-         cpfolder=${de1folder}
-      elif [ "$2" == "sockit" ]; then
-         cpfolder=${sockitfolder}
-      fi
-   fi
-   if [ "$cpfolder" != "" ]; then
-      sudo mkdir -p /mnt/rootfs
-      sudo mount $1 /mnt/rootfs
-      sudo cp -fv boot_files/${cpfolder}/socfpga* /mnt/rootfs/boot
-      sync
-      sudo umount /mnt/rootfs
-#     sudo mkdir -p /mnt/boot
-#     sudo mount -o rw -o uid=1000,gid=1000 $1 /mnt/boot
-#     sudo cp -fv boot_files/socfpga* /mnt/boot
-#     sync
-#     sudo umount /mnt/boot
-      sync
-      echo "operation finished sucessfully"
+   if [ "$1" == "-c" ]; then
+      for folder in $projects
+      do
+         echo "Will attempt to copy Boot files from: ${projdirname}/${projects}/${folder}"
+         mkdir -p boot_files/$folder
+
+         cp -v ../$projdirname/$folder/soc_system.dtb boot_files/$folder/socfpga.dtb
+         cp -v ../$projdirname/$folder/soc_system.dts boot_files/$folder/socfpga.dts
+         cp -v ../$projdirname/$folder/output_files/soc_system.rbf boot_files/$folder/socfpga.rbf
+         echo "Boot files copied to: ${folder}"
+      done
    else
-      echo "wrong board install option use: [/dev/sdxx] [nano or de1 or sockit]"
+      lsblk
+      echo ""
+      echo "files will be installed on $1"
+      echo ""
+      if [ "$2" != "" ]; then
+         if [ "$2" == "nano" ]; then
+            cpfolder=${nanofolder}
+         elif [ "$2" == "de1" ]; then
+            cpfolder=${de1folder}
+         elif [ "$2" == "sockit" ]; then
+            cpfolder=${sockitfolder}
+         fi
+      fi
+      if [ "$cpfolder" != "" ]; then
+         sudo mkdir -p /mnt/rootfs
+         sudo mount $1 /mnt/rootfs
+         sudo cp -fv boot_files/${cpfolder}/socfpga* /mnt/rootfs/boot
+         sync
+         sudo umount /mnt/rootfs
+#        sudo mkdir -p /mnt/boot
+#        sudo mount -o rw -o uid=1000,gid=1000 $1 /mnt/boot
+#        sudo cp -fv boot_files/socfpga* /mnt/boot
+#        sync
+#        sudo umount /mnt/boot
+         sync
+         echo "operation finished sucessfully"
+      else
+         echo "wrong board install option use: [/dev/sdxx] [nano or de1 or sockit]"
+      fi
    fi
 else
    lsblk
@@ -59,6 +60,8 @@ else
    echo "will only copy files to boot_folder"
    echo "to install files to sd"
    echo "use: [/dev/sdxx] [nano | de1 | sockit]"
+   echo ""
+   echo "use: [-c] to copy files from mksocfpga Quartus project folders"
    echo ""
 fi
 
---
+++ prg_fpga.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+modprobe -r gpio_altera
+
+echo 0 > /sys/class/fpga-bridge/fpga2hps/enable
+echo 0 > /sys/class/fpga-bridge/hps2fpga/enable
+echo 0 > /sys/class/fpga-bridge/lwhps2fpga/enable
+
+dd of=/dev/fpga0 if=/boot/socfpga.rbf
+
+echo 1 > /sys/class/fpga-bridge/fpga2hps/enable
+echo 1 > /sys/class/fpga-bridge/hps2fpga/enable
+echo 1 > /sys/class/fpga-bridge/lwhps2fpga/enable
+
+modprobe gpio_altera
