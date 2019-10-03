b4aec5720c930957dc7039a5ca4b1fafa24d79b3
Michael Brown <producer@holotronic.dk>
2019-10-03 13:47:28 +0200

    Make buster image smaller, add BOOT.BIN v1 and v2 for both U96 revisions
    Signed-off-by: Michael Brown <producer@holotronic.dk>

--- SD-Image-Gen/build-all.sh
+++ SD-Image-Gen/build-all.sh
@@ -607,15 +607,8 @@ assemble_full_sd_img() {
                         sudo cp ${XIL_BOOT_FILES_LOC}/peta_built/images/linux/BOOT.BIN ${1}
                         sudo cp ${XIL_BOOT_FILES_LOC}/peta_built/images/linux/image.ub ${1}
                     else
-##                        if [ "${4}" == "bullseye" ]; then
-                            sudo cp ${XIL_BOOT_FILES_LOC}/BOOT.BIN* ${1}
-                            sudo cp ${XIL_BOOT_FILES_LOC}/image.ub ${1}
-#                            sudo cp /home/mib/avnet-ultra96-rev1.dtb ${1}
-#                            sudo cp /home/mib/Image ${1}
-##                        else
-##                            sudo cp ${XIL_BOOT_FILES_LOC}/BOOT.BIN* ${1}
-##                            sudo cp ${XIL_BOOT_FILES_LOC}/image.ub ${1}
-##                        fi
+                        sudo cp ${XIL_BOOT_FILES_LOC}/BOOT.BIN* ${1}
+                        sudo cp ${XIL_BOOT_FILES_LOC}/image.ub ${1}
                     fi
                     echo "MSG: Unmounting boot partition"
                     unmount_binded ${1}
